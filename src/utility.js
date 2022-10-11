a36604751b25a7e743d595e3d7b854a88730160e
Aosmiles <arlin.smiles@gmail.com>
2022-10-11 12:08:00 -0700

    import cloudinary utility

--- src/utility.js
+++ src/utility.js
@@ -1,10 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary';
 
-cloudinary.config({
-  cloud_name: 'arlin-ortiz',
-  api_key: import.meta.env.CLOUDINARY_API_KEY,
-  api_secret: import.meta.env.CLOUDINARY_API_SECRET,
-  secure: true
-});
-
-console.log("cloudinary config set");
+export function setupCloudinary() {
+  cloudinary.config({
+    cloud_name: 'arlin-ortiz',
+    api_key: import.meta.env.CLOUDINARY_API_KEY,
+    api_secret: import.meta.env.CLOUDINARY_API_SECRET,
+    secure: true
+  });
+  console.log("cloudinary config set");
+}
